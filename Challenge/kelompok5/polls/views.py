f7bb5d0898aabffd3867000ebf5c903ff66db1c8
Fansqiee <affansiddiqie98@gmail.com>
2025-08-08 08:55:18 +0000

    update jenkinsfile

--- Challenge/kelompok5/polls/views.py
+++ Challenge/kelompok5/polls/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, YTH AFGAN ;) This is the polls index page.")
+    return HttpResponse("Hello, TEMAN-TEMAN TERSAYANG ;) This is the polls index page.")

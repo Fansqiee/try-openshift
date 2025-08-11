6dc27e354f1a69546ef1cb9f01290d1712bc95cd
Fansqiee <affansiddiqie98@gmail.com>
2025-08-11 08:49:47 +0000

    fix haproxy erorr

--- Challenge/kelompok5/LoadTest.js
+++ Challenge/kelompok5/LoadTest.js
@@ -1,11 +1,11 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const BASE_URL = 'https://django-megumismine-dev.apps.rm1.0a51.p1.openshiftapps.com/polls/'; // Ganti dengan Route kamu
+const BASE_URL = 'https://django-megumismine-dev.apps.rm1.0a51.p1.openshiftapps.com'; 
 
 export let options = {
   stages: [
-    { duration: '1m', target: 100 },  // 10 users selama 1 menit
+    { duration: '1m', target: 1000 },  // 10 users selama 1 menit
   ],
 };
 

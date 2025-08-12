1525c57aa99378011463fa0e5afd0dcfdc99be7b
Fansqiee <affansiddiqie98@gmail.com>
2025-08-12 03:39:10 +0000

    new FE

--- Challenge/kelompok5/LoadTest.js
+++ Challenge/kelompok5/LoadTest.js
@@ -1,11 +1,11 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const BASE_URL = 'https://django-megumismine-dev.apps.rm1.0a51.p1.openshiftapps.com'; 
+const BASE_URL = 'http://haproxy-megumismine-dev.apps.rm1.0a51.p1.openshiftapps.com'; 
 
 export let options = {
   stages: [
-    { duration: '1m', target: 1000 },  // 10 users selama 1 menit
+    { duration: '1m', target: 10000 },  // 10 users selama 1 menit
   ],
 };
 
